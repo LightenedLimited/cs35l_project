7cd397be0442be13b8f51f73920a24fde1256888
LightenedLimited <macnguyenanh@gmail.com>
2023-11-19 14:30:53 -0800

    added non encrypted password comparison

---
+++ server/schema/user.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose'); 
+const Schema = mongoose.Schema; 
+
+//basing off of https://www.mongodb.com/blog/post/password-authentication-with-mongoose-part-1
+const userSchema = new Schema({
+    username: {
+        type: String, 
+        required: true, 
+        index: {
+            unique: true
+        }
+    },
+    password: {
+        type: String, 
+        required: true
+    }
+})
+
+userSchema.methods.comparePasswords = function(otherPassword, cb) {
+    cb(this.password == otherPassword); 
+}
+
+const userModel = mongoose.model("User", userSchema); 
+module.exports = {userModel}; 
