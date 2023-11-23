b2b76f52b156823b66183fecb2ab8ed79e8e9d83
LightenedLimited <macnguyenanh@gmail.com>
2023-11-23 01:55:02 -0800

    able to upload documents to server

--- server/routes/pdfs.js
+++ server/routes/pdfs.js
@@ -3,6 +3,19 @@ var router = express.Router();
 
 const pdfs = require("../schema/pdfs"); 
 const { validLogin } = require("../middleware/cookieManager"); 
+var path = require('path'); 
+var multer = require('multer'); 
+
+const upload = multer({
+    storage: multer.diskStorage({
+        destination: (req, file, cb) => {
+            cb(null, path.join(__dirname, '../uploads/'));
+        },
+        filename: (req, file, cb) => {
+            cb(null, Date.now() + ".pdf"); 
+        }
+    })
+})
 
 router.use(validLogin()); 
 
@@ -12,5 +25,10 @@ router.get('/search', function(req, res, next) {
         res.json(results); 
     })
 })
+
+router.post('/upload', upload.single("pdf"), function(req, res, next) {
+    res.sendStatus(200); 
+}); 
+
 
 module.exports = router; 
