4d49881e1195acade087c7eecf8fae506bcead2c
LightenedLimited <macnguyenanh@gmail.com>
2023-11-23 02:40:22 -0800

    added uploading form

--- server/routes/pdfs.js
+++ server/routes/pdfs.js
@@ -27,7 +27,25 @@ router.get('/search', function(req, res, next) {
 })
 
 router.post('/upload', upload.single("pdf"), function(req, res, next) {
-    res.sendStatus(200); 
+    const newPDF = new pdfs.Test({
+        user_upload_id: req.session.userid, //validated by validLogin
+        path: req.file.path, 
+        subject: req.body.subject, 
+        professor: req.body.professor,
+        title: req.body.title,
+        upload_date: new Date(), 
+        class: req.body.class, 
+        quarter: req.body.quarter, 
+        year: req.body.year,
+        test_type: req.body.test_type, 
+        has_solution: req.body.has_solution,
+        users_notes: req.body.users_notes ? req.body.users_notes : ""
+    })
+    newPDF.save().then((success) => {
+        res.sendStatus(200); 
+    }).catch(err => {
+        res.status(400).send(err) 
+    })
 }); 
 
 
--- server/schema/pdfs.js
+++ server/schema/pdfs.js
@@ -2,23 +2,52 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema; 
 
 const testSchema = new Schema({
-    user_upload_id: Schema.Types.ObjectId, 
-    path: String, 
-    subject: String,
-    professor: String, 
-    title: String,
-    upload_date: Date, 
-    class: String, 
+    user_upload_id:  {
+        type: Schema.Types.ObjectId, 
+        required: true
+    }, 
+    path: {
+        type: String,
+        required: true
+    }, 
+    subject: {
+        type: String,
+        required: true
+    },
+    professor: {
+        type: String, 
+        required: true
+    }, 
+    title: {
+        type: String, 
+        required: true
+    },
+    upload_date: {
+        type: Date,
+        required: true
+    }, 
+    class: {
+        type: String,
+        required: true
+    }, 
     quarter: {
         type: String,
-        enum: ["Fall", "Winter", "Spring", "Summer"]
+        enum: ["Fall", "Winter", "Spring", "Summer"],
+        required: true
     }, 
-    year: Number,
+    year: {
+        type: Number,
+        required: true
+    },
     test_type: {
         type: String,
-        enum: ["Quiz", "Midterm", "Final", "Practice Quiz", "Practice Midterm", "Practice Final"]
+        enum: ["Quiz", "Midterm", "Final", "Practice Quiz", "Practice Midterm", "Practice Final"],
+        required: true
     },
-    has_solution: Boolean, 
+    has_solution: {
+        type: Boolean,
+        required: true
+    }, 
     users_notes: String
    }
 )
