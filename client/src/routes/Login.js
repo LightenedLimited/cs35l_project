8c572d8b1af8605c871a6b15e5c1ab09ab3b6531
LeroyBG <leroylightning@g.ucla.edu>
2023-11-24 16:35:42 -0800

    login page dynamically switches to sign up

--- client/src/routes/Login.js
+++ client/src/routes/Login.js
@@ -10,11 +10,35 @@
 // FOR NOW, I'm gonna have the email checked when the user presses submit button -- easiest to impleent
 import { useState } from 'react'
 import { globals } from '../globals'
-
+// login stores state of all forms
+// when you click submit, it checks the value of the email form to see whether that email is in use
 
 export function Login(){
+    let [loginState, setLoginState] = useState(true) // true initially, false if email not found
+
     let [emailInput, setEmailInput] = useState('')
     let [pwInput, setPwInput] = useState('')
+    let [confPwInput, setConfPwInput] = useState('')
+
+    /* async */ function checkEmail(e){
+        e.preventDefault()
+        console.log(emailInput)
+        const url = globals.sever_url
+
+        // TODO: check that exists is actually bool when this route is implemented
+        const res = /* await */ fetch(`${url}/users/validate`, {
+            method: 'PUT',
+            body: {
+                email: emailInput,
+            },
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        })
+        loginState = false // change when routing gets set up
+        setLoginState(loginState)
+        return false // change
+    }
 
     return (
         <>
@@ -22,35 +46,24 @@ export function Login(){
             <form>
                 <input type='email' placeholder='email' onChange={e => setEmailInput(e.target.value)}></input>
                 <input type='password' placeholder='password' onChange={e => setPwInput(e.target.value)}></input>
-                <SubmitBtn email_input={emailInput}/>
+                <ConfPassword loginSt={loginState} setConfInp={setConfPwInput} />
+                <SubmitBtn valid={checkEmail} loginSt={loginState} setLoginSt={setLoginState}/>
+                <p>If the specified account doesnt exist, you will be prompted to make one</p>
             </form>
         </>
     )
 }
 
-function SubmitBtn(email_input){
-    let [loginState, setLoginState] = useState(true) // true initially, false if email not found
-
-    async function checkEmail(e){
-        e.preventDefault()
-        console.log(e)
-        console.log(email_input)
-        const url = globals.sever_url
-
-        // TODO: check that exists is actually bool when this route is implemented
-        const exists = await fetch(`${url}/users/validate`, {
-            method: 'PUT',
-            body: {
-                email: email_input,
-            },
-            headers: {
-                'Content-Type': 'application/json',
-            },
-        })
-        setLoginState(exists)
-    }
-
-    return (
-        <button onClick={checkEmail}>{loginState ? 'LOGIN' : 'SIGN UP'}</button>
+function SubmitBtn({valid, loginSt, setLoginSt} /* function */){
+    return ( // doesnt change button value!
+        <button onClick= { (e) => setLoginSt( valid(e) ) } > {loginSt ? 'LOGIN' : 'SIGN UP'} </button>
     )
+}
+
+function ConfPassword({loginSt, setConfInp}){
+    if (!loginSt){
+        return (
+            <input type='password' placeholder='Confirm password' onChange={e => setConfInp(e.target.value)}></input>
+        )
+    }
 }
