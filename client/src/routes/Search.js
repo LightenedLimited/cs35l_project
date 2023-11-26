c352aa0e5f0b1c59a244bef5b81ab3e2d9955202
Jack Thompson <jackthompson03@g.ucla.edu>
2023-11-26 00:14:48 -0800

    added course search

--- client/src/routes/Search.js
+++ client/src/routes/Search.js
@@ -3,6 +3,7 @@ import { useState } from 'react'
 export function Search() {
     const [subject, setSubject] = useState("");
     const [professor, setProfessor] = useState("")
+    const [course, setCourse] = useState("")
     const [query, setQuery] = useState("")
 
     return (
@@ -11,27 +12,35 @@ export function Search() {
             <h3>FILTERS: Quarter,...</h3>
             <form>
                 <input
+                    type='Subject'
+                    placeholder='ex. Math'
+                    value={subject}
+                    onChange={(e) => setSubject(e.target.value)}
+                ></input>
+                <input
                     type='Professor'
                     placeholder='ex. Eggert'
                     value={professor}
                     onChange={(e) => setProfessor(e.target.value)}
                 ></input>
                 <input
-                    type='Subject'
-                    placeholder='ex. Math'
-                    value={subject}
-                    onChange={(e) => setSubject(e.target.value)}
+                    type='Course'
+                    placeholder='ex. CS35L'
+                    value={course}
+                    onChange={(e) => setCourse(e.target.value)}
                 ></input>
-                <button type="button" onClick={(e) => setQuery(generateQuery(subject, professor))} value="SEARCH"/>
+                <button type="button" value="SEARCH" onClick={(e) => console.log(generateQuery(subject, professor, course))}/>
             </form>
         </>
         )
 }
 
-function generateQuery(subject, professor) {
+function generateQuery(subject, professor, course) {
     let temp = "";
+    temp += subject;
+    temp += ", ";
     temp += professor;
     temp += ", ";
-    temp += subject;
+    temp += course;
     return temp;
 }
