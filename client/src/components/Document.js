1c0146ce1acb45db831134074457cfccf0f5870e
LeroyBG <leroylightning@g.ucla.edu>
2023-12-03 18:57:45 -0800

    copied nams document code

---
+++ client/src/components/Document.js
@@ -0,0 +1,19 @@
+import React from "react";
+import image from '../styles/images/pdf-icons.png';
+import '../styles/Documents.css';
+
+export function Document({data}){
+    console.log(data)
+    let {link,name} = data;
+    return(
+        <div className="document-container">
+            <img
+                alt="pdficons"
+                class="pdficons"
+                src={image}
+                onClick={() => window.open(link, "_blank")}
+            />
+            <div class="document-name">{name}</div>
+        </div>
+    )
+}
